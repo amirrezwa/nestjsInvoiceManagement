808ac7d3e8b7b3ae9c14751a0774c2b516dfbf52
amir-reza <amirrezawnoori@gmail.com>
2025-01-15 11:32:38 +0330

    rename: branch name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'invoice',
+      password: 'secret123',
+      database: 'invoice_db',
+      entities: [__dirname + '/**/*.entity{.ts,js}'],
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
